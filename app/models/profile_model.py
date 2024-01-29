846eeaad985acd1221fbb83b604fc73032ef2291
Godswill Nasara <godswillnlm@gmail.com>
2024-01-29 21:35:18 +0100

    Update: disable profile model

--- app/models/profile_model.py
+++ app/models/profile_model.py
@@ -1,47 +1,47 @@
-import enum
-from datetime import datetime
-import uuid
-from pydantic import UUID4
-from sqlalchemy import (
-    ForeignKey,
-    String,
-    Enum,
-    Uuid,
-)
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+# import enum
+# from datetime import datetime
+# import uuid
+# from pydantic import UUID4
+# from sqlalchemy import (
+#     ForeignKey,
+#     String,
+#     Enum,
+#     Uuid,
+# )
+# from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-# from typing import Optional
+# # from typing import Optional
 
-from app.core.db_config import Base
-
-
-class GenderEnum(enum.Enum):
-    male = "m", "male"
-    female = "f", "female"
-    others = "o", "others"
-    none = "n", "none"
+# from app.core.db_config import Base
 
 
-now = datetime.utcnow
+# class GenderEnum(enum.Enum):
+#     male = "m", "male"
+#     female = "f", "female"
+#     others = "o", "others"
+#     none = "n", "none"
 
 
-class Profile(Base):
-    __tablename__ = "profiles"
+# now = datetime.utcnow
 
-    id: Mapped[UUID4] = mapped_column(
-        Uuid(as_uuid=True, native_uuid=True), primary_key=True, default=uuid.uuid4
-    )
-    firstname: Mapped[str] = mapped_column(String(length=50), index=True)
-    lastname: Mapped[str] = mapped_column(String(length=50), index=True)
-    # gender: Mapped[GenderEnum] = mapped_column(default=GenderEnum.none)
-    # date_of_birth: Mapped[datetime | None] = mapped_column(default=now)
 
-    # relationships
-    user_id = mapped_column(ForeignKey("users.id"))
-    user = relationship("User", back_populates="profile")
+# class Profile(Base):
+#     __tablename__ = "profiles"
 
-    created_at: Mapped[datetime] = mapped_column(default=now)
-    modified_at: Mapped[datetime] = mapped_column(default=now, onupdate=now)
+#     id: Mapped[UUID4] = mapped_column(
+#         Uuid(as_uuid=True, native_uuid=True), primary_key=True, default=uuid.uuid4
+#     )
+#     firstname: Mapped[str] = mapped_column(String(length=50), index=True)
+#     lastname: Mapped[str] = mapped_column(String(length=50), index=True)
+#     # gender: Mapped[GenderEnum] = mapped_column(default=GenderEnum.none)
+#     # date_of_birth: Mapped[datetime | None] = mapped_column(default=now)
 
-    def __repr__(self) -> str:
-        return f"Profile(id={self.id}, firstname={self.firstname}, lastname={self.lastname})"
+#     # relationships
+#     user_id = mapped_column(ForeignKey("users.id"))
+#     user = relationship("User", back_populates="profile")
+
+#     created_at: Mapped[datetime] = mapped_column(default=now)
+#     modified_at: Mapped[datetime] = mapped_column(default=now, onupdate=now)
+
+#     def __repr__(self) -> str:
+#         return f"Profile(id={self.id}, firstname={self.firstname}, lastname={self.lastname})"
