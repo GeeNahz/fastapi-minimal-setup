5326ac0e21ff38aec38f18a4f9809a26ed908df6
Godswill Nasara <godswillnlm@gmail.com>
2024-01-29 22:13:36 +0100

    Update: add base url to test client

--- app/utils/conftest.py
+++ app/utils/conftest.py
@@ -1,8 +1,13 @@
 import pytest
 from fastapi.testclient import TestClient
+from app.core.config import settings
 from app.main import app
 
-@pytest.fixture(scope='module')
+
+@pytest.fixture(scope="module")
 def client():
-    with TestClient(app) as c:
+    with TestClient(
+        app,
+        base_url=f"http://127.0.0.1:8000{settings.API_V1_STR}",
+    ) as c:
         yield c
