21ef470f34374967da930c734b3b5395f0bcec29
Godswill Nasara <godswillnlm@gmail.com>
2024-01-29 22:24:26 +0100

    Refactor: remove print statement

--- app/api/api_v1/handlers/tests/test_user.py
+++ app/api/api_v1/handlers/tests/test_user.py
@@ -2,6 +2,5 @@ from app.utils.conftest import client
 
 
 def test_get_all_users(client):
-    print(response)
-    response = client.get('/all/')
+    response = client.get('/user/')
     assert response.status_code == 200
